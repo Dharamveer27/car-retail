ba745f61056908672475629fe46f82ca06fe1160
Dharamveer27 <52097441+Dharamveer27@users.noreply.github.com>
2022-07-13 23:37:49 +0530

    login and registration done , using JWT

--- car-retail/server.js
+++ car-retail/server.js
@@ -1,10 +1,9 @@
-import express, { json, urlencoded} from 'express';
+import express, { json, urlencoded } from 'express';
 import path, { join } from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import { config } from 'dotenv';
-
-
+import globalErrorHandler from './controlers/error.controlers.js'
 import { default as mongoose } from 'mongoose';
 import usersRouter from './routes/usersRoute.js';
 import indexRouter from './routes/index.js';
@@ -13,11 +12,11 @@ var app = express();
 config();
 //connection to db
 
-mongoose.connect(process.env.CONNECTION_URL , {
-    useNewUrlParser: true
-  }).then(() => {
-    console.log("Connected to db");
-  })
+mongoose.connect(process.env.CONNECTION_URL, {
+  useNewUrlParser: true
+}).then(() => {
+  console.log("Connected to db");
+})
   .catch((err) => console.log(err.message));
 
 
@@ -31,6 +30,7 @@ app.use(express.static(join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.use(globalErrorHandler);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
